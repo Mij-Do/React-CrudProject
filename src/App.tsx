73af22b65d1e8c886182529e847e97f6c874ce56
ahmed <ahmedmijdo2@gmail.com>
2025-05-09 09:21:44 +0300

    add api for get products && function to reduce the size of the description

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import ProductCard from "./components/ProductCard"
 import Button from "./components/ui/Button"
+import { productsList } from "./data"
 
 function App() {
+
+  // render
+  const renderProducts = productsList.map(product => <ProductCard key={product.id} product={product}/>)
 
   return (
     <main className="container mx-auto">
       <Button className="bg-indigo-500 hover:bg-indigo-400 my-5">Add New Product</Button>
       <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-2 grid-cols-1 ">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {renderProducts}
       </div>
     </main>
   )
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,17 +1,25 @@
+import type { Iproduct } from "../interface";
+import { txtLength } from "../utils/functions";
 import Image from "./Image";
 import Button from "./ui/Button";
 
-const ProductCard = () => {
+interface IProps {
+    product: Iproduct;
+}
+
+const ProductCard = ({product}: IProps) => {
+    const {title, description, imageURL, price} = product;
     return (
         <div className="border border-gray-400 rounded-md flex flex-col justify-between">
             <Image 
-                src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" 
-                alt="Girl with a Camera"
-                className="w-full rounded-md mb-5"
-                />
+                src={imageURL}
+                alt={title}
+                className="w-35 h-35 m-auto rounded-md mb-5"
+            />
+
             <div className="p-1">
-                <h2 className="mb-5">Product Title</h2>
-                <p className="mb-5">Product Description</p>
+                <h2 className="mb-5">{title}</h2>
+                <p className="mb-5">{txtLength(description, 80)}</p>
 
                 <div className="flex space-x-2 mb-5">
                     <span className="w-5 h-5 rounded-full bg-indigo-500 block cursor-pointer"></span>
@@ -20,11 +28,11 @@ const ProductCard = () => {
                 </div>
 
                 <div className="flex justify-between items-center mb-5">
-                    <span className="block text-indigo-400">$5,000</span>
+                    <span className="block text-indigo-400">${price}</span>
                     <Image 
-                    src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" 
-                    alt="Girl with a Camera"
-                    className="rounded-full w-10 h-10 object-contain"
+                        src={imageURL}
+                        alt={title}
+                        className="w-10 h-10 rounded-full object-contain"
                     />
                 </div>
 
